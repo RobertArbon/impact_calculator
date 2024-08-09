2c5996642a3b951fb9c2e4e240b9b99fa1c359b3
Lex O'brien <lex.e.obrien@gmail.com>
2024-08-09 11:34:03 +0100

    added bootstrap stylings

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from dash import Dash, html, dcc, Input, Output, callback
+import dash_bootstrap_components as dbc
 
 DAYS_PER_YEAR = 365
 ELEC_GHG_DENSITY = 0.21
 GAS_GHG_DENSITY = 0.18  
 KG_PER_TONNE = 1000
 
-app = Dash(__name__)
+app = Dash(__name__, 
+           external_stylesheets=[dbc.themes.BOOTSTRAP
+                                 ])
 
 server = app.server
 
