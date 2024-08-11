7acb5259e9e941bff34d9e1826e556f64abb02f5
Lex O'brien <lex.e.obrien@gmail.com>
2024-08-11 16:56:46 +0100

    config file

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+import rich
+
+class Config(object): 
+    DEBUG = True
+    TESTING = False
+    HOST = os.environ['HOST']
+    USER = os.environ['USER']
+    PASSWORD = os.getenv('PASSWORD', None)
+    PORT = os.environ['PORT']
+    TABLE_CONN = os.getenv('TABLE_CONN', None)
+    DB_NAME = os.environ['DB_NAME']
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{HOST}:{PORT}/{DB_NAME}"
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT=True
+
+
