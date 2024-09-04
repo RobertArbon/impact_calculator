e8e87ab59d36c384d1aeb6e08dbe2f69fe1ac516
Lex O'brien <lex.e.obrien@gmail.com>
2024-09-04 13:20:04 +0100

    udpated config

--- config.py
+++ config.py
@@ -4,16 +4,21 @@ import rich
 class Config(object): 
     DEBUG = True
     TESTING = False
-    HOST = os.environ['HOST']
-    USER = os.environ['USER']
+    HOST = os.getenv('HOST')
+    USER = os.getenv('USER')
     PASSWORD = os.getenv('PASSWORD', None)
-    PORT = os.environ['PORT']
+    PORT = os.getenv('PORT')
     TABLE_CONN = os.getenv('TABLE_CONN', None)
-    DB_NAME = os.environ['DB_NAME']
+    DB_NAME = os.getenv('DB_NAME')
     DATABASE_URL = f"postgresql://{HOST}:{PORT}/{DB_NAME}"
 
 class DevelopmentConfig(Config):
     DEVELOPMENT=True
     APP_PORT = 8888
+
+class ProductionConfig(Config):
+    DEVELOPMENT = False
+    DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 
